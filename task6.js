2184b36e3c3366a6b3089d839a63eb85ef47b24d
aimanshahid93 <108936343+aimanshahid93@users.noreply.github.com>
2022-07-20 12:34:02 +0530

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,112 @@
+//represintin the class movie using a constructor
+//class Movie{
+//constructor(title,studio,rating){
+    //setting the values
+  //this.title=title;
+  //this.studio=studio;
+//this.rating=rating;
+//}
+  //getspecification(){
+    //let data=(`
+    //title=${this.title}
+    //studio=${this.studio}
+    //rating=${this.rating}
+    //`)
+    //return data;
+  //}
+//}
+//creating an instance of Movie
+//let film=new Movie("spiderman","WarnerBros","PG13")
+//console.log(film.getspecification());
+/*output
+title=spiderman
+studio=WarnerBros
+rating=PG13
+*/
+//constuctuct the class property rating to PG as a default value
+//class Movie{
+//constructor(title,studio,rating="PG"){
+//setting the values
+    //this.title=title;
+    //this.studio=studio;
+    //this.rating=rating;
+    //}
+     // getspecification(){
+       // let data=(`
+       // title=${this.title}
+        //studio=${this.studio}
+        //rating=${this.rating}
+        //`)
+        
+       // return data;
+        
+      //}
+    //}
+    //creating an instance of Movie
+    //let film=new Movie("spiderman","WarnerBros",)
+    //console.log(film.getspecification());
+    /*output
+title=spiderman
+studio=WarnerBros
+rating=PG
+*/
+//write a method getpg and take an array
+//class  movie{
+  //constructor(title,studio,rating){
+    //this.title=title;
+    //this.studio=studio;
+    //this.rating=rating;
+  //}
+
+//getPG(){
+//let data=(`
+//title=${this.title}
+//studio=${this.studio}
+//rating=${this.rating}
+//`)
+
+//return data;
+//}
+//}
+
+//let movie1=new movie({title:"spiderman",studio:"warnerBros",rating:"PG"});
+//let movie2=new movie({title:"spiderman2",studio:"warnerBros",rating:"PG"});
+//let movie3=new movie({title:"spiderman3",studio:"warnerBros",rating:"R"});
+//let movie4=new movie({title:"high school musical",studio:"elon",rating:"R"});
+//let movie5=new movie({title:"princess diaries",studio:"Disney",rating:"R"});
+
+//var array=[movie1,movie2,movie3,movie4,movie5];
+//let result=array.filter(obj=>obj.rating=="PG");
+//console.log(result);
+  
+    /*output
+0: {title: 'spiderman', studio: 'warnerBros', rating: 'PG'}
+1: {title: 'spiderman2', studio: 'warnerBros', rating: 'PG'}
+length: 2
+     */
+
+
+
+//class Movie{
+//constructor(title,studio,rating){
+// setting the values
+//this.title=title;
+//this.studio=studio;
+//this.rating=rating;
+//}
+  //getspecification(){
+    //let data=(`
+    //title=${this.title}
+    //studio=${this.studio}
+    //rating=${this.rating}
+    //`)
+    //return data;
+ // }
+//}
+//creating an instance of Movie
+//let film=new Movie("Casino Royale","Eon Production","PG13")
+//console.log(film.getspecification());
+/*ouput
+title=Casino Royale
+    studio=Eon Production
+    rating=PG13*/
